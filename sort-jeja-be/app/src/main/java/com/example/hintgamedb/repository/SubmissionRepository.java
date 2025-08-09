abb1e6b8d8b7a1369e48947d357e0ef1671c8702
root <root@localhost.localdomain>
2025-08-09 13:50:58 +0900

    rulebook info modify

--- sort-jeja-be/app/src/main/java/com/example/hintgamedb/repository/SubmissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hintgamedb.repository;
-
-import com.example.hintgamedb.domain.Submission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-    List<Submission> findByIsCorrectTrue();
-}
